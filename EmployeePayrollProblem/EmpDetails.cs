da82527df82c4afbd0bd78a0e719ac8cefbadc19
unknown <kinekarpallavi21@gmail.com>
2022-04-28 18:29:25 +0530

    Connect to database

---
+++ EmployeePayrollProblem/EmpDetails.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollProblem
+{
+     public class EmpDetails
+    {
+           static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeePayroll;Integrated Security=True;";
+            static SqlConnection sqlconnection = new SqlConnection(connectionString);
+            public void EstablishConnection()
+            {
+                if (sqlconnection != null && sqlconnection.State.Equals(ConnectionState.Closed))
+                {
+                    try
+                    {
+                        sqlconnection.Open();
+                    }
+                    catch (Exception)
+                    {
+                        throw new EmpExceptioncs(EmpExceptioncs.ExceptionType.Connection_Failed, "connection failed");
+
+                    }
+                }
+            }
+            public void CloseConnection()
+            {
+                if (sqlconnection != null && sqlconnection.State.Equals(ConnectionState.Open))
+                {
+                    try
+                    {
+                        sqlconnection.Close();
+                    }
+                    catch (Exception)
+                    {
+                        throw new EmpExceptioncs(EmpExceptioncs.ExceptionType.Connection_Failed, "connection failed");
+                    }
+                }
+            }
+        }
+    }
+
+
---
+++ EmployeePayrollProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace EmployeePayrollProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in EmployeePayroll");
+            EmpDetails empDetails = new EmpDetails();
+            empDetails.EstablishConnection();
+            empDetails.CloseConnection();
+        }
+    }
+}
